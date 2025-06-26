4c8d55e2162409e7cd4e13815e77e5cf044a086d
devvMiguel <miguelgalvao_galvao@hotmail.com>
2025-06-26 11:39:49 -0300

    feat: adicionando entity funcionario

---
+++ src/funcionarios/entities/funcionarios.entity.ts
@@ -0,0 +1,29 @@
+import { IsNotEmpty } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tb_funcionarios' })
+export class Funcionario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ type: 'decimal', precision: 8, scale: 2 })
+  salario: number;
+
+  @IsNotEmpty()
+  @Column({ length: 20, nullable: false })
+  cargo: string;
+
+  @IsNotEmpty()
+  @CreateDateColumn({ type: 'timestamp', nullable: false })
+  data_admissao: Date;
+}
