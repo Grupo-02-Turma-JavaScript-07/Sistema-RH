b9a73facc27592604adec41ce8f83a7af315aabc
Natalia Taira <nataliagtaira@gmail.com>
2025-06-26 10:56:31 -0300

    Adiciona dependências de Funcionario, FuncionarioController, FuncionarioService

--- src/funcionarios/funcionario.module.ts
+++ src/funcionarios/funcionario.module.ts
@@ -1,5 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Funcionario } from "./entities/funcionarios.entity";
+import { FuncionarioController } from "./controllers/funcionario.controller";
+import { FuncionarioService } from "./services/funcionario.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Funcionario])],
