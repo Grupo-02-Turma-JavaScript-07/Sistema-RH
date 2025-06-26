011b1a54f0e7e3e65d8095cf2c6fd8751e87b31d
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-06-26 10:58:07 -0300

    feat: adicionando AppModule, conexão com o Mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_rhFlow',
+      //Precisamos adicionar a Entidade abaixo feito pelo Miguel
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
